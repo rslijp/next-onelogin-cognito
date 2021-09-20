e4afad752c274df0c49573273f0696eb056a6bd3
rslijp <renzo.slijp@softcause.nl>
2021-09-20 12:35:31 +0200

    updated pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
-import '../styles/globals.css'
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+Amplify.configure({
+  ...awsconfig,
+  ssr: true}
+);
+
+Auth.configure(awsconfig);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
